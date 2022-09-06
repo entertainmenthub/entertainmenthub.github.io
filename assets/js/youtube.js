b2ba55b97f9efe94f80cf8415345e67dc1f5ebd5
NatKingCoder <111017653+NatKingCoder@users.noreply.github.com>
2022-09-06 17:41:31 -0400

    Create youtube.js

---
+++ assets/js/youtube.js
@@ -0,0 +1,21 @@
+var buttonEl = document.getElementById("button");
+    var getVideo = function () {
+      var url = document.getElementById("url").value;
+      if (url == "Paste Link Here..." || url == "") {
+        alert("Error: No link inputted");
+      }
+      var char = "";
+      var i = 0;
+      while (char != "=") {
+        char = url.charAt(i);
+        i++;
+      }
+      var vidCode = "";
+      while (i < url.length) {
+        vidCode = vidCode + url.charAt(i);
+        i++;
+      }
+      var embedLink = "https://www.youtube.com/embed/" + vidCode;
+      document.getElementById("vid").src = embedLink;
+    }
+buttonEl.addEventListener("click", getVideo);
