3d32d52bd2eee076abf9e65ced0d477edfeeea5d
NatKingCoder <111017653+NatKingCoder@users.noreply.github.com>
2022-10-15 14:36:09 -0400

    Create play.js

---
+++ assets/js/play.js
@@ -0,0 +1,29 @@
+    const karts = {
+      name: "Smash Karts",
+      url: "https://www.crazygames.com/embed/smash-karts",
+    };
+    const br = {
+      name: "Build Royale.io",
+      url: "https://www.crazygames.com/embed/buildroyale-io",
+    };
+    const lol = {
+      name: "1v1 LOL",
+      url: "https://www.crazygames.com/embed/1v1-lol",
+    };
+    const chess = {
+      name: "Master Chess",
+      url: "https://www.crazygames.com/embed/master-chess",
+    };
+    const ctrtt = {
+      name: "Cut the Rope: Time Travel",
+      url: "https://www.crazygames.com/embed/cut-the-rope-time-travel",
+    };
+    
+    
+    const gameId = localStorage.getItem("gameId");
+    const setUp = function () {
+      document.getElementById("game").src = gameId.url;
+      document.getElementById("title").textContent = gameId.name;
+      document.getElementById("head").textContent = "eHub - " + gameId.name;
+    }
+    setUp();
