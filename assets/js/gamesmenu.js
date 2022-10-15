9800c0f3e8e4302df6efadaffcefdecc90ce5f37
NatKingCoder <111017653+NatKingCoder@users.noreply.github.com>
2022-10-14 21:52:36 -0400

    Update gamesmenu.js

--- assets/js/gamesmenu.js
+++ assets/js/gamesmenu.js
@@ -2,7 +2,7 @@
       const 1v1El = document.getElementById("1v1");
       const chessEl = document.getElementById("chess");
       const ctrttEl = document.getElementById("ctr-tt");
-      const slopeEl = document.getElementById("slope");
+      const kartsEl = document.getElementById("karts");
       const launchGame = function (gameId) {
         localStorage.setItem("gameId", gameId);
         window.location.href = "/play";
@@ -11,4 +11,4 @@
       1v1El.addEventListener("click", function(){launchGame("1v1");});
       chessEl.addEventListener("click", function(){launchGame("chess");});
       ctrttEl.addEventListener("click", function(){launchGame("ctrtt");});
-      slopeEl.addEventListener("click", function(){launchGame("slope");});  
+      kartsEl.addEventListener("click", function(){launchGame("slope");});  
